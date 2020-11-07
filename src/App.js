56b4ff4d22aaa7ec0ff5c6eb2ac1284d4446c66b
stephin007 <stephinreji123@gmail.com>
2020-11-07 11:13:03 +0530

    Added tha navbar using material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import PropTypes from 'prop-types';
+import {Button,
+    AppBar,
+    Toolbar,
+    Typography,
+    CssBaseline,
+    useScrollTrigger,
+    Box,
+    Container,
+    Fab,
+    Zoom
+} from "@material-ui/core";
+import {KeyboardArrowUp, SportsCricket} from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
+import "./App.css"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+
+}));
+
+function ScrollTop(props) {
+    const { children, window } = props;
+    const classes = useStyles();
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = (event) => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+        if (anchor) {
+            anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+    };
+
+    return (
+        <Zoom in={trigger}>
+            <div onClick={handleClick} role="presentation" className={classes.root}>
+                {children}
+            </div>
+        </Zoom>
+    );
+}
+
+ScrollTop.propTypes = {
+    children: PropTypes.element.isRequired,
+    window: PropTypes.func,
+};
+
+const App = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <React.Fragment>
+            <CssBaseline />
+            <AppBar>
+                <Toolbar>
+                    <Typography variant="h3" style={{fontFamily:"Nunito"}}>CricLive<SportsCricket/></Typography>
+                </Toolbar>
+            </AppBar>
+            <Toolbar id="back-to-top-anchor" />
+
+            <ScrollTop {...props}>
+                <Fab color="secondary" size="small" aria-label="scroll back to top">
+                    <KeyboardArrowUp />
+                </Fab>
+            </ScrollTop>
+        </React.Fragment>
     </div>
   );
 }
