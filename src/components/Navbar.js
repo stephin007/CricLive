33a02bf5265a624a99f0502a53c253588867e0d7
stephin007 <stephinreji123@gmail.com>
2020-11-08 12:03:46 +0530

    Edited the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,85 +1,22 @@
 import React from 'react'
+import {Link} from "react-router-dom" 
 import PropTypes from 'prop-types';
-import {Button,
-    AppBar,
-    Toolbar,
-    Typography,
-    CssBaseline,
-    useScrollTrigger,
-    Box,
-    Container,
-    Fab,
-    Zoom
-} from "@material-ui/core";
-import {KeyboardArrowUp, SportsCricket} from '@material-ui/icons'
-import { makeStyles } from '@material-ui/core/styles';
-
-// import "../App.css"
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        position: 'fixed',
-        bottom: theme.spacing(2),
-        right: theme.spacing(2),
-    },
-
-}));
-
-function ScrollTop(props) {
-    const { children, window } = props;
-    const classes = useStyles();
-    const trigger = useScrollTrigger({
-        target: window ? window() : undefined,
-        disableHysteresis: true,
-        threshold: 100,
-    });
-
-    const handleClick = (event) => {
-        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
-
-        if (anchor) {
-            anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
-        }
-    };
-
-    return (
-        <Zoom in={trigger}>
-            <div onClick={handleClick} role="presentation" className={classes.root}>
-                {children}
-            </div>
-        </Zoom>
-    );
-}
-
-ScrollTop.propTypes = {
-    children: PropTypes.element.isRequired,
-    window: PropTypes.func,
-};
 
 const Navbar = (props) => {
     return (
-        <div className="App">
-            <React.Fragment>
-                <CssBaseline />
-                <AppBar>
-                    <Toolbar>
-                        <Typography
-                            variant="h4"
-                            style={{fontFamily:"Nunito"}}>
-                            CricLive
-                            <SportsCricket
-                                style={{fontSize:"25px"}}/>
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <Toolbar id="back-to-top-anchor" />
-
-                <ScrollTop {...props}>
-                    <Fab color="secondary" size="small" aria-label="scroll back to top">
-                        <KeyboardArrowUp />
-                    </Fab>
-                </ScrollTop>
-            </React.Fragment>
+        <div className="navbar bg-primary">
+            <h1>
+                <i className="fa fa-hashtag"/>
+                CricLive
+            </h1>
+            <ul>
+                {/* <li>
+                <Link to="/about">About</Link>
+                </li> */}
+                <li>
+                <a href="https://github.com/stephin007/CricLive" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
+                </li>
+            </ul>
         </div>
     )
 }
