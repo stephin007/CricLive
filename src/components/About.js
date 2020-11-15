df08e5b574c0cdccff9f5c393cf4a8547a565495
stephin007 <stephinreji123@gmail.com>
2020-11-15 20:53:29 +0530

    Bumped the version

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
             <h1>About this app</h1>
             <p className="about-para">This is a React app for getting latest Cricket stats</p>
             <p className="bg-dark padd">
-                <strong>Version:</strong> 1.0.0
+                <strong>Version:</strong> 1.0.10
             </p>
         </div>
     )
