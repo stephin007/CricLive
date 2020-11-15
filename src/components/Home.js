5be4c7ebdbf33abf1e71d187c5d9e6b02fa69a95
stephin007 <stephinreji123@gmail.com>
2020-11-15 20:08:21 +0530

    Added no matches found text when search gives no result

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,29 @@ import MyCard from "./MyCard"
 import Spinner from "../img/spinner.gif"
 
 const Home = ({filteredMatches, loading}) => {
+    // if (filteredMatches.length === 0) {
+    //     return <h3 style={{textAlign : "center"}}>Please Add a Contact</h3>
+    // }
+
     return (
         <Fragment>
         {
             loading === false ? (
                 <div className="container">
                     {
-                        filteredMatches.map((match) =>(
-                            <MyCard key={match.unique_id} match={match}/>
-                        ))  
-                    }
+                        filteredMatches.length !== 0 ? (
+                            filteredMatches.map((match) =>(
+                                <MyCard key={match.unique_id} match={match}/>
+                            ))
+                        ) : 
+                        (
+                            <h3 style={{textAlign : "center", color: "grey"}}>No Matches Found, Try searching Twenty20</h3>
+                        )
+                    }    
+
                 </div>
             ) : (
-                <img src={Spinner} alt="Loading..." style={{display: "flex", justifyContent: "center", alignItems: "center", marginLeft:"550px", marginTop:"200px"}} />
+                <img src={Spinner} alt="Loading..." style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} />
             )     
         }
         </Fragment>
