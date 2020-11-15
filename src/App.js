35df348e90838de77eb78bc695a9da4cbce8fede
stephin007 <stephinreji123@gmail.com>
2020-11-15 21:27:46 +0530

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
 
 import Navbar from "./components/Navbar";
 import About from "./components/About";
@@ -16,17 +15,11 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
       width: "35ch",
     },
-  },
-  textField: {
-    width: "300px",
-    marginLeft: "-1px",
-  },
+  }
 }));
 
 const App = () => {
-  const classes = useStyles();
   const [matches, setMatches] = useState([]);
-  const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -39,32 +32,16 @@ const App = () => {
       .catch((error) => console.log(error));
   }, []);
 
-  const filteredMatches = matches.filter((match) => {
-    return match.type.toLowerCase().includes(search.toLowerCase());
-  });
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <div className="container search">
-          <h1>Latest updates</h1>
-          <form className={classes.root} noValidate>
-            <TextField
-              onChange={(e) => setSearch(e.target.value)}
-              id="outlined-basic"
-              label="Search"
-              variant="outlined"
-              placeholder="Search Match Type"
-              className={classes.textField}
-            />
-          </form>
-        </div>
         <Switch>
           <Route
             exact
             path="/"
             render={(props) => (
-              <Home {...props} filteredMatches={filteredMatches} loading={loading} />
+              <Home {...props} matches={matches} loading={loading} />
             )}
           />
           <Route exact path="/about" component={About} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,46 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 import MyCard from "./MyCard"
 
 import Spinner from "../img/spinner.gif"
+const useStyles = makeStyles((theme) => ({
+    root: {
+        "& > *": {
+          margin: theme.spacing(1),
+          width: "35ch",
+        },
+      },
+      textField: {
+      width: "300px",
+      marginLeft: "-1px",
+    },
+  }));
 
-const Home = ({filteredMatches, loading}) => {
-    // if (filteredMatches.length === 0) {
-    //     return <h3 style={{textAlign : "center"}}>Please Add a Contact</h3>
-    // }
+const Home = ({matches, loading}) => {
+    const classes = useStyles();
+    const [search, setSearch] = useState("");
+
+    const filteredMatches = matches.filter((match) => {
+        return match.type.toLowerCase().includes(search.toLowerCase());
+      });
+    
 
     return (
         <Fragment>
+        <div className="container search">
+          <h1>Latest updates</h1>
+          <form className={classes.root} noValidate>
+            <TextField
+              onChange={(e) => setSearch(e.target.value)}
+              id="outlined-basic"
+              label="Search"
+              variant="outlined"
+              placeholder="Search Match Type"
+              className={classes.textField}
+            />
+          </form>
+        </div>
         {
             loading === false ? (
                 <div className="container">
